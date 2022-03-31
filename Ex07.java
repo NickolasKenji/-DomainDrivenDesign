3d9096ab6af6639d2e4b7aad19f076d685c654d4
Nickolas Kenji <101301519+NickolasKenji@users.noreply.github.com>
2022-03-31 19:59:30 -0300

    Exercício 07 Arrumado

--- Ex07.java
+++ Ex07.java
@@ -1,10 +1,41 @@
 package ddd;
-
+import java.util.Scanner;
 public class Ex07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		
+		double a, b, c, d, e, troco, total, pago;
+		
+		System.out.printf("Digite o primeiro valor: ");
+		a = ler.nextDouble();
+		
+		System.out.printf("Digite o segundo valor: ");
+		b = ler.nextDouble();
+		
+		System.out.printf("Digite o terceiro valor: ");
+		c = ler.nextDouble();
+		
+		System.out.printf("Digite o quarto valor: ");
+		d = ler.nextDouble();
+		
+		System.out.printf("Digite o quinto valor: ");
+		e = ler.nextDouble();
+		
+		System.out.printf("Digite o valor pago: ");
+		pago = ler.nextDouble();
+		
+		total = a + b + c + d + e;
+		troco = a + b + c + d + e - pago;
+		
+		if (troco < 0)
+		troco = troco * (-1);
+		
+		System.out.printf("O total é %.1f", total);
+		System.out.printf("O troco é %.1f", troco);
 
-	}
-
+		}
+	
+			//Nicks o Mais Brabo
+	
 }
